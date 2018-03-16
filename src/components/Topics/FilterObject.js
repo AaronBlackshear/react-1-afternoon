7b719aa1e7f32221da7d6c6c205a6b7d2e20406c
Aaron Blackshear <theshiftybapple@gmail.com>
2018-03-16 15:52:32 -0500

    Started Filter Object

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class FilterObject extends Component{
+    constructor(){
+        super()
+        this.state = {
+            unFilteredArray: [{student: 'Aaron', age: 18, size: 100},{student: 'Jarid', age: 24, size: 130},{student: 'Joe', age: 22, size: 140}],
+            userInput: '',
+            filteredArray: []
+        };
+    }
+
+    inputChange(val){
+        this.setState({
+            userInput: val
+        })
+    }
+
+    
 
     render(){
         
         return(
-            <p>Filter Object</p>
+            <div>
+                <h4 classNam='puzzleBox filterObjectPB'>Fliter Object</h4>
+                <span className='puzzleText'></span>
+                <input className='inputLine' onChange={e => {this.inputChange(e.target.value)}} ></input>
+                <button className='confirmationButton' onClick={} ></button>
+                <span calssName='resultsBox filterObjectPB'></span>
+            </div>
         )
     }
 }
